f6deadad92ab4e92f1baa93335eab76790a3a487
Marcin Wysokiński <m.wysokinski@gloo.pl>
2023-11-03 10:22:19 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { revalidateTag } from "next/cache";
 import { useEffect, useState } from "react";
 
 
@@ -7,12 +8,13 @@ export default function Home() {
   const [data, setData] = useState<{data: number}>();
 
   useEffect(() => {
-    fetch("/api").then((res) => res.json()).then((value) => setData(value))
+    fetch("/api", {next: {tags: ["a"]}}).then((res) => res.json()).then((value) => setData(value))
   }, []);
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       {data?.data}
+      <button type="button" onClick={() => revalidateTag("a")}>revalidate</button>
     </main>
   )
 }
