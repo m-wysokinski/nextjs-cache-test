62e917b0629509e9d297deb9689b4dcac437f01d
Marcin Wysokiński <m.wysokinski@gloo.pl>
2023-11-03 11:15:43 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,14 @@
-export default async function Home() {
-  const data = await fetch("http://localhost:3000/api").then((res) => res.json())
+"use client";
+
+import { useEffect, useState } from "react";
+
+export default function Home() {
+  const [data, setData] = useState<{data: number}>();
+
+  useEffect(() => {
+    fetch("/api").then((res) => res.json()).then((value) => setData(value))
+    fetch("/api", {next: {tags: ["a"]}}).then((res) => res.json()).then((value) => setData(value))
+  }, []);
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
