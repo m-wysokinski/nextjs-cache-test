ab9258f41cf5fdb51940c08e4846789203d6539a
Marcin Wysokiński <m.wysokinski@gloo.pl>
2023-11-03 16:13:25 +0100

    Update route.ts

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,13 +1,11 @@
-export async function GET(r: Request) {
-  console.log(r.cache);
-  
+import { NextRequest } from "next/server";
 
+export async function GET(r: NextRequest) {
   let a = 0;
 
   for (let i = 0; i < 1_000_000_000; i++) {
     a += 1;
   }
 
-  return Response.json({ data: a })
+  return Response.json({ data: a, request: r.nextUrl.searchParams.get("a") })
 }
-
