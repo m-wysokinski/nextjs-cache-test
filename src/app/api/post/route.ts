8dc4dd69bd86c544930251f2a3dd4424793d0d49
Marcin Wysokiński <m.wysokinski@gloo.pl>
2023-11-03 09:17:01 +0100

    Test

--- src/app/api/post/route.ts
+++ src/app/api/post/route.ts
@@ -1,4 +1,4 @@
-export async function GET() {
+export async function POST() {
   let a = 0;
 
   for (let i = 0; i < 1_000_000_000; i++) {
@@ -7,4 +7,3 @@ export async function GET() {
 
   return Response.json({ data: a })
 }
-
